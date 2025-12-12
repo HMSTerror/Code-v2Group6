cc1344e871d50187f1732c9ec0f6c19fb9a1a9db
DestoryerType055 <thunderunderthestars@gmail.com>
2025-12-12 15:56:10 +0800

    Modify

--- src/hub/DeviceGroup.java
+++ src/hub/DeviceGroup.java
@@ -1,27 +1,38 @@
 package hub;
 
+import devices.Device;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
- * A simple device group which stores device names.
+ * A simple device group which stores device objects directly.
+ * Refactored to use Device objects instead of String names.
  */
 public class DeviceGroup {
     private final String name;
-    private final Set<String> deviceNames = new HashSet<>();
+    private final Set<Device> devices = new HashSet<>();
 
     public DeviceGroup(String name) {
         this.name = name;
     }
 
     public String getName() { return name; }
-    public void addDevice(String deviceName) { deviceNames.add(deviceName); }
-    public void removeDevice(String deviceName) { deviceNames.remove(deviceName); }
-    public Set<String> getDeviceNames() { return Collections.unmodifiableSet(deviceNames); }
+
+    public void addDevice(Device device) {
+        devices.add(device);
+    }
+
+    public void removeDevice(Device device) {
+        devices.remove(device);
+    }
+
+    public Set<Device> getDevices() {
+        return Collections.unmodifiableSet(devices);
+    }
 
     @Override
     public String toString() {
-        return "DeviceGroup{" + name + ", size=" + deviceNames.size() + "}";
+        return "DeviceGroup{" + name + ", size=" + devices.size() + "}";
     }
 }
